0b8dc8007adb00f68504549a526379d12e53e5af
Eric Cadwell <ericmcadwell@gmail.com>
2019-09-05 01:22:46 -0700

    changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,89 @@
-var targetScore;
-var totalScore;
-var winCount;
-var lossCount;
-var redCount;
-var blueCount;
-var yellowCount;
-var greenCount;
-
-
-$(document).ready(function() {
-
-    var targetScore = Math.floor(Math.random()*101+19);
+$( document ).ready(function(){
+    var Random=Math.floor(Math.random()*101+19)
     
-    $("#numberToGet").text(targetScore);
+    $('#numberToGet').text(Random);
+   
+    var num1= Math.floor(Math.random()*11+1)
+    var num2= Math.floor(Math.random()*11+1)
+    var num3= Math.floor(Math.random()*11+1)
+    var num4= Math.floor(Math.random()*11+1)
     
-    var totalScore= 0;
-    var wins = 0;
+    var userTotal= 0; 
+    var wins= 0;
     var losses = 0;
     
-    $("#numberWins").text(wins);
-    $("#numberLosses").text(losses);
-    
-    function getCrystalhandler(redCount, blueCount, yellowCount, greenCount) {
-        return function() {
-            
-        }
-    }
-    function reset() {
-        targetScore = Math.floor(Math.random()*120+1);
-        console.log(targetScore);
-        $("#numberToGet").text(targetScore);
-        var redCount = Math.floor(Math.random()*12+1);
-        var blueCount = Math.floor(Math.random()*12+1);
-        var greenCount = Math.floor(Math.random()*12+1);
-        var yellowCount = Math.floor(Math.random()*12+1);
-        totalScore = 0;
-        $("#score").text(totalScore);
-    }
-    
-    function winner() {
-        alert("You Won!!");
-        wins++;
-        $("#numberWins").text(wins);
-        reset();
-    }
-    
-    function loser() {
-        alert("You Lose!!");
-        losses++;
-        $("#numberLosses").text(losses);
-        reset();
-    }
-    
-    $("#image1").on("click", function() {
-        totalScore = totalScore + num1;
-        console.log("New totalScore " + totalScore);
-        $("#score").text(totalScore);
-    
-        if (totalScore === random) {
-            winner()
-        }
-    
-        else if (totalScore > random) {
-            loser()
+  $('#blessedWins').text(wins);
+  $('#crushingLosses').text(losses);
+  
+  function reset(){
+        Random=Math.floor(Math.random()*101+19);
+        console.log(Random)
+        $('#numberToGet').text(Random);
+        num1= Math.floor(Math.random()*11+1);
+        num2= Math.floor(Math.random()*11+1);
+        num3= Math.floor(Math.random()*11+1);
+        num4= Math.floor(Math.random()*11+1);
+        userTotal= 0;
+        $('#finalScore').text(userTotal);
         } 
-    })
-
-    $("#red").on("click", )
-});
+  
+  function yay(){
+    wins++; 
+    $('#blessedWins').text(wins);
+    reset();
+  }
+  
+  function loser(){
+    losses++;
+    $('#crushingLosses').text(losses);
+    reset()
+  }
+ 
+    $('#one').on ('click', function(){
+      userTotal = userTotal + num1;
+      console.log("New userTotal= " + userTotal);
+      $('#finalScore').text(userTotal); 
+            
+          if (userTotal == Random){
+            yay();
+          }
+          else if ( userTotal > Random){
+            loser();
+          }   
+    })  
+    $('#two').on ('click', function(){
+      userTotal = userTotal + num2;
+      console.log("New userTotal= " + userTotal);
+      $('#finalScore').text(userTotal); 
+          if (userTotal == Random){
+            yay();
+          }
+          else if ( userTotal > Random){
+            loser();
+          } 
+    })  
+    $('#three').on ('click', function(){
+      userTotal = userTotal + num3;
+      console.log("New userTotal= " + userTotal);
+      $('#finalScore').text(userTotal);
+  
+            if (userTotal == Random){
+            yay();
+          }
+          else if ( userTotal > Random){
+            loser();
+          } 
+    })  
+    $('#four').on ('click', function(){
+      userTotal = userTotal + num4;
+      console.log("New userTotal= " + userTotal);
+      $('#finalScore').text(userTotal); 
+        
+            if (userTotal == Random){
+            yay();
+          }
+          else if ( userTotal > Random){
+            loser();
+          }
+    });   
+  }); 
